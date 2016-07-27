5ecdcc94ff657a130ddba317e7db82423641bdc2
Irakli Lekishvili <l3kishvili@gmail.com>
2016-07-27 20:17:19 +0400

    shopify added

---
+++ config/initializers/shopify_app.rb
@@ -0,0 +1,6 @@
+ShopifyApp.configure do |config|
+  config.api_key = "a366cbafaccebd2f615aebdfc932fa1c"
+  config.secret = "8750306a895b3dbc7f4136c2ae2ea293"
+  config.scope = "read_orders, read_products"
+  config.embedded_app = true
+end
