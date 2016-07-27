2f1583e4740c2390c3d5238368515796be5fefd7
Irakli Lekishvili <l3kishvili@gmail.com>
2016-07-27 20:21:15 +0400

    shopify configured

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :shopify,
     ShopifyApp.configuration.api_key,
     ShopifyApp.configuration.secret,
+    redirect_uri: 'https://localhost/auth/shopify/callback',
+    callback_url: 'https://localhost/auth/shopify/callback',
     scope: ShopifyApp.configuration.scope
 end
--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,6 +1,6 @@
 ShopifyApp.configure do |config|
-  config.api_key = "a366cbafaccebd2f615aebdfc932fa1c"
-  config.secret = "8750306a895b3dbc7f4136c2ae2ea293"
+  config.api_key = ENV['SHOPIFY_API_KEY']
+  config.secret = ENV['SHOPIFY_SECRET']
   config.scope = "read_orders, read_products"
   config.embedded_app = true
 end
