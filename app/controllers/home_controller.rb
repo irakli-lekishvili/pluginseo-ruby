da7a6797a3d1ec0721428470a49beba7d0c73ae0
Irakli Lekishvili <l3kishvili@gmail.com>
2016-07-29 12:33:34 +0400

    added query param on redirect

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
-    redirect_to 'http://localhost:8080'
+    shop = ShopifyAPI::Shop.current
+    redirect_to "http://localhost:8080?url=#{shop.myshopify_domain}"
     # @products = ShopifyAPI::Product.find(:all, :params => {:limit => 10})
   end
 end
